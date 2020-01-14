09401e1eaa351a2988c30f0970b205855a1fab42
Student <example@example.com>
2020-01-14 11:52:32 -0800

    add clickable terms and fading definitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-
+$("#JS").click(function() {
+  $("#JS-def").fadeToggle();
+  $("#JS").fadeToggle();
+  // $("#JS-def").fadeOut();
+});
+$("#JS-def").click(function() {
+  $("#JS").fadeToggle();
+  $("#JS-def").fadeToggle();
+  // $("#JS-def").fadeOut();
+});
 
 
 });
